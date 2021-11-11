da9d66e813c9fbaa0306b3c09b485420f0157e91
bh0307 <84839207+bh0307@users.noreply.github.com>
2021-11-11 17:39:16 +0900

    타겟 위치

--- Campfire/Assets/Scripts/GameScene/playermove.cs
+++ Campfire/Assets/Scripts/GameScene/playermove.cs
@@ -14,31 +14,19 @@ public class playermove : MonoBehaviour
     Vector3 moveVelocity = Vector3.zero;
     float moveSpeed = 0.5f; // 움직이는 속도
     public Vector3 target;
+    public Vector3 target_x;
+    public Vector3 target_z;
 
     // Use this for initialization
     void Start()
     {
         target = new Vector3(3.64f, -2f, 3.78f);
         animator = gameObject.GetComponent<Animator>();
+        target_x = transform.position;
+        target_z = transform.position;
     }// Update is called once per frame
     void Update()
     {
-        if (LeftMove)
-        {
-           movingLeft();
-        }
-        if (RightMove)
-        {
-            MovingRight();
-         }
-        if (UpMove)
-        {
-            MovingUp();
-         }
-        if (DownMove)
-         {
-            MovingDown();
-        }
         if (transform.position == target)
         {
             LeftMove = false;
@@ -46,35 +34,43 @@ public class playermove : MonoBehaviour
             UpMove = false;
             DownMove = false;
             animator.SetInteger("Movingdirection", 0);
+
         }
     }
-    void MovingRight()
+    public void MovingRight()
     {
-            target =transform.position + new Vector3(1.1f, 0, 0);
-            animator.SetInteger("Movingdirection", 1);
-           // moveVelocity = new Vector3(+0.10f, 0, 0);
-            transform.position = Vector3.MoveTowards(transform.position, target, 1f * Time.deltaTime);
+        target += new Vector3(1.1f, 0, 0);
+        //animator.SetInteger("Movingdirection", 1);
+        // moveVelocity = new Vector3(+0.10f, 0, 0);
+        //transform.position = Vector3.MoveTowards(transform.position, target, 1f * Time.deltaTime);
+        Debug.Log(target);
     }
-    void movingLeft()
+    public void movingLeft()
     {
-            target =transform.position + new Vector3(-1.1f, 0, 0);
-            animator.SetInteger("Movingdirection", 1);
-           // moveVelocity = new Vector3(+0.10f, 0, 0);
-            transform.position = Vector3.MoveTowards(transform.position, target, 1f * Time.deltaTime);
+        target += new Vector3(-1.1f, 0, 0);
+        //animator.SetInteger("Movingdirection", 1);
+        // moveVelocity = new Vector3(+0.10f, 0, 0);
+        //transform.position = Vector3.MoveTowards(transform.position, target, 1f * Time.deltaTime);
+        Debug.Log(target);
     }
-    void MovingUp()
+    public void MovingUp()
     {
-    target =transform.position + new Vector3(0, 1.1f, 0);
-            animator.SetInteger("Movingdirection", 1);
-           // moveVelocity = new Vector3(+0.10f, 0, 0);
-            transform.position = Vector3.MoveTowards(transform.position, target, 1f * Time.deltaTime);
+        target += new Vector3(0, 0, 1.1f);
+        //animator.SetInteger("Movingdirection", 1);
+        // moveVelocity = new Vector3(+0.10f, 0, 0);
+        //transform.position = Vector3.MoveTowards(transform.position, target, 1f * Time.deltaTime);
+        Debug.Log(target);
     }
-    void MovingDown()
+    public void MovingDown()
     {
-    target =transform.position + new Vector3(0, -1.1f, 0);
-            animator.SetInteger("Movingdirection", 1);
-           // moveVelocity = new Vector3(+0.10f, 0, 0);
-            transform.position = Vector3.MoveTowards(transform.position, target, 1f * Time.deltaTime);
+        target += new Vector3(0, 0, -1.1f);
+        //animator.SetInteger("Movingdirection", 1);
+        // moveVelocity = new Vector3(+0.10f, 0, 0);
+        //transform.position = Vector3.MoveTowards(transform.position, target, 1f * Time.deltaTime);
+        Debug.Log(target);
     }
-
+    public void Move()
+    {
+        transform.position = Vector3.MoveTowards(transform.position, target, 1f * Time.deltaTime);
+    }
 }
